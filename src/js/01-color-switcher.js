a883a3e2986061fec24724b17f22c7860fe17dde
shuriuss <104681622+shuriuss@users.noreply.github.com>
2022-08-11 15:50:45 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startEL = document.querySelector('[data-start]');
+const stopEL = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+
+startEL.addEventListener('click', () => {
+  if (!timerId) {
+    timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+});
+
+stopEL.addEventListener('click', () => {
+  clearInterval(timerId);
+  timerId = null;
+});
+
+function getRandomHexColor() {
+  console.log(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
